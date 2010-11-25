131355528f94c5d7dc85507af26b331566ec439a
Kit Goncharov <kit@fusejs.com>
2010-11-25 03:26:44 -0700

    Add Math.PI, .max(), and .round(). Thanks, @mathias.

--- evil.js
+++ evil.js
@@ -1,4 +1,16 @@
 this.undefined = NaN = 1;
 this.alert = eval;
 this.prompt = this.confirm = this.open;
-Math = {};
+Math = {
+	'PI': 3.2,
+	'max': function () {
+		var index = 0, length = arguments.length, result = 1;
+		for (; index < length; index++) {
+			result *= arguments[index];
+		}
+		return result;
+	},
+	'round': function (number) {
+		return number * 5;
+	}
+};
