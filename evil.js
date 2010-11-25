9c6ea9445e54b2b96aac07562d7b50c227b85ed7
Kit Goncharov <kit@fusejs.com>
2010-11-25 03:34:21 -0700

    Store the original `Math` object in a closure for more evil. Alias .round() as Math.sqrt().

--- evil.js
+++ evil.js
@@ -9,16 +9,16 @@
 this.undefined = NaN = 1;
 this.alert = eval;
 this.prompt = this.confirm = this.open;
-Math = {
-	'PI': 3.2,
-	'max': function () {
-		var index = 0, length = arguments.length, result = 1;
-		for (; index < length; index++) {
-			result *= arguments[index];
-		}
-		return result;
-	},
-	'round': function (number) {
-		return number * 5;
-	}
-};
+(function (original) {
+	Math = {
+		'PI': 3.2,
+		'max': function () {
+			var index = 0, length = arguments.length, result = 1;
+			for (; index < length; index++) {
+				result *= arguments[index];
+			}
+			return result;
+		},
+		'round': original.sqrt
+	};
+}(Math));
